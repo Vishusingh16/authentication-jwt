8567d44d3b250465b1a03952250cff8b27ad6f3f
Vaishnav Singh <vaishnavsingh162@gmail.com>
2024-12-18 00:28:21 +0530

    new commit made

--- controllers/userController.go
+++ controllers/userController.go
@@ -14,6 +14,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/vishusingh16/authentication-jwt/database"
+	"github.com/vishusingh16/authentication-jwt/models"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -26,7 +27,41 @@ import (
 
  func VerifyPassword()
 
- func Signup()
+ func Signup()gin.HandlerFunc{
+
+	return func(c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error" : err.Error()})
+			return
+
+		}
+		validationErr :=validate.Struct(user)
+		 if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		 }
+		 count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		 defer cancel()
+		 if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError,gin.H{"error" : "error while checking email" })
+		 }
+		 	count , err := userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+			defer cancel()
+			if err != nil{
+				log.Panic(err)
+					c.JSON(http.StatusInternalServerError, gin.H{"error" : "error occured while checking phone number"})
+			}
+			if count>0{
+				c.JSON(http.StatusInternalServerError, gin.H{"error" :"user already exists"} )
+			}
+			user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	}
+ }
 
 
  func Login()
@@ -46,6 +81,14 @@ import (
 
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		var user models.User
+	    err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+
+		defer cancel()
+		if err!= nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error" : err.Error})
+			return 
+		}
+		c.JSON(http.StatusOK, user)
 		
 	}
  }
